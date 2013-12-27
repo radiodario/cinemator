f4aa2a61936e1042cf6ef1ffeb696962b84c08e7
Dario Villanueva <dario.villanueva@gmail.com>
2013-12-27 23:48:20 +0100

    notifications

---
+++ app/scripts/modules/notification.js
@@ -0,0 +1,35 @@
+define(['jquery', 'backbone'], function ($, Backbone) {
+
+  var Notifications = Backbone.View.extend({
+    el : 'div.notifications',
+    initialize : function(options) {
+      Backbone.on('notification', this.notify, this)
+    },
+
+    notify : function(message) {
+      // stop all animations
+      this.$el.stop(true, true)
+      var pos = { 
+        top: 10, 
+        left: window.innerWidth/2 - this.$el.width()/2
+      };
+      if (typeof message === "string") {
+        this.$el
+          .html(message)
+        
+      }
+
+      if (message.hasOwnProperty('pos')) {
+        this.$el.html(message.text)
+        pos = message.pos;
+      }
+
+      this.$el.css(pos).show().fadeOut(1500);
+
+    }
+
+  })
+
+  return Notifications;
+
+})
